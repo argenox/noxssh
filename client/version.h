660bacae29c4af40e10b740de1ad17c24c10410f
Gustavo Litovsky <gustavo@argenox.com>
2026-05-10 03:40:01 -0500

    ver: bumps version

--- client/version.h
+++ client/version.h
@@ -14,9 +14,9 @@
 /** @brief Minor version number. */
 #define NOXSSH_VERSION_MINOR 1
 /** @brief Patch version number. */
-#define NOXSSH_VERSION_PATCH 10
+#define NOXSSH_VERSION_PATCH 12
 
 /** @brief Version string for display (e.g. "0.1.0"). */
-#define NOXSSH_VERSION_STRING "0.1.10"
+#define NOXSSH_VERSION_STRING "0.1.12"
 
 #endif /* NOXSSH_VERSION_H */
